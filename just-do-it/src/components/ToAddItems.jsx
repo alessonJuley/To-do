863a9fdb5c5b7dc3c40bf39180168b3568f1353c
alessonJuley <alessonabao@gmail.com>
2024-12-16 01:37:19 +1300

    added elements in return inside component file

--- just-do-it/src/components/ToAddItems.jsx
+++ just-do-it/src/components/ToAddItems.jsx
@@ -1,10 +1,18 @@
+import React, {useState} from 'react';
+
 function ToAddItems(){
-          return(
-                    <form>
-                              <input type="text" name="item" placeholder="item"></input>
+          
+
+          return(<div> 
+                    <form className="to-do-form">
+                              <input type="text" name="item" placeholder="Add a task" ></input>
                               <input type="submit" value="Submit"></input>
                     </form>
-          );
+                    <label className="to-do-label">Here are thing you need to complete by this week:</label>
+                    <div className="tasks">
+                             
+                    </div>
+          </div>);
 }
 
 export default ToAddItems;
