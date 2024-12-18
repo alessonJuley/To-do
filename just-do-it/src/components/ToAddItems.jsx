4084e120d120c314165256b577c420207dbec7a7
alessonJuley <alessonabao@gmail.com>
2024-12-19 02:16:40 +1300

    Add and delete function complete

--- just-do-it/src/components/ToAddItems.jsx
+++ just-do-it/src/components/ToAddItems.jsx
@@ -2,9 +2,9 @@ import React, {useState} from 'react';
 
 function ToAddItems(){
           
-
           const [tasks, setTasks] = useState(["Eat", "Shower", "Sleep"]);
 
+          // Create
           function handleAddTask(){
                     const newTask = document.getElementById("tasksInput").value;
                     
@@ -18,37 +18,38 @@ function ToAddItems(){
                     document.getElementById("tasksInput").value = "";
           }
 
-          function handleRemoveTask(index){
-                    setTasks(tasks.filter((_, i) => i !== index));
-          }
-
+          // Update
           function handleEditTask(index){
                     console.log("Edit task");
           }
 
+          // Delete
+          function handleRemoveTask(index){
+                    setTasks(tasks.filter((_, i) => i !== index));
+          }
+
+          // Put completed task at the last 
+          function handleDoneTask(index){
+                    setTasks();
+          }
+
           return(<div>
-                    <input type="text" className="task-textbox" id="taskInput" placeholder="Some task"/>
+                    <input type="text" className="task-textbox" id="tasksInput" placeholder="Some task"/>
                     <button className="task-button" onClick={handleAddTask}>Add tasks</button>
 
-                    <div className="list-container">
+                    {/* <div className="list-container">
                               <input type="checkbox"/>
                               <div className="task-item">item</div>
                               <button className="task-action" onClick={handleRemoveTask}>Remove Task</button>
                               <button className="task-action" onClick={handleEditTask}>Edit Task</button>
-                    </div>
+                    </div> */}
 
                     <div className="list-container">
-                              <input type="checkbox"/>
-                              <div className="task-item">iteasdfasdfsadm</div>
-                              <button className="task-action" onClick={handleRemoveTask}>Remove Task</button>
-                              <button className="task-action" onClick={handleEditTask}>Edit Task</button>
-                    </div>
-
-                    <div className="list-container">
-                              <input type="checkbox"/>
-                              <div className="task-item">iteasdfasm</div>
-                              <button className="task-action" onClick={handleRemoveTask}>Remove Task</button>
-                              <button className="task-action" onClick={handleEditTask}>Edit Task</button>
+                              <ul>
+                                        {tasks.map((task, index) => <li key={index}> <input type="checkbox"/> {task} <button className="task-action" onClick={() => handleRemoveTask(index)}>Remove Task</button> <button className="task-action" onClick={handleEditTask}>Edit Task</button>
+                                                                      
+                                                            </li>)}
+                              </ul>
                     </div>
           </div>);
 }
