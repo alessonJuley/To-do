4d8dbeddd0e8afe6f81f5de588dfb1cb6ba486c7
alessonJuley <alessonabao@gmail.com>
2025-02-12 01:46:43 +1300

    Modified preventing duplicate input, clean up code

--- to-do-app/src/Content.jsx
+++ to-do-app/src/Content.jsx
@@ -28,10 +28,11 @@ const Content = () => {
 
           const [tasks, setTasks] = useState([]);
 
-          // useEffect here
           // when the component mounts, execute this 
           useEffect(() => {
-                    const localData = localStorage.getItem('tasks');
+                    
+                    const localData = localStorage.getItem('tasksLocalStorage');
+
                     if(localData){
                               setTasks(JSON.parse(localData));
                     }
@@ -39,8 +40,10 @@ const Content = () => {
 
           // when tasks array value changes, execute this
           useEffect(() => {
+                    
                     if(tasks.length > 0){
-                              localStorage.setItem('tasks', JSON.stringify(tasks));
+                              localStorage.setItem('tasksLocalStorage', JSON.stringify(tasks));
+
                     }
 
           }, [tasks]);
@@ -53,26 +56,37 @@ const Content = () => {
 
                     // take values from form input
                     const data = new FormData(event.target);
-                    const newTaskItem = data.get("taskItem");
-                    const newTask = {
+                    const newTaskItem = data.get("taskItem");         // taskItem found under input name="taskItem"
+                    const newTask = {                                 // create object variable and set values taken from form
                               id: randomID(),
                               taskItem: newTaskItem,
-                              isChecked: false,
-                    };
+                              isChecked: false,                       // set tasks as not checked when being added first
+                    }
 
-                    // validate if task being added is empty
+                    // validate if task being added is an empty input
                     if(newTask.taskItem.trim() !== ""){
 
-                              // validate if task being added is a duplicate
-                              tasks.map((task) => {
-                                        if(newTaskItem === task.taskItem){
-                                                  alert("Task is already in the list.");
-                                        }
-                                        else{
+                              // check if task being added is the first item in an empty tasks array
+                              // we check this because mapping won't work in an empty array :)
+                              if(tasks.length === 0){
+                                        setTasks(prevTask => [...prevTask, newTask]);
+                              }
+                              else{
+                                        // validate if task being added is a duplicate in tasks array
+                                        let flag = true;
+
+                                        tasks.map((task) => {
+                                                  if(newTaskItem === task.taskItem){
+                                                            alert("Task is already in the list.");
+
+                                                            flag = false;
+                                                  }
+                                        });
+
+                                        if(flag == true){
                                                   setTasks(prevTask => [...prevTask, newTask]);
-                                                  console.log("newTaskItem: ", newTaskItem);
                                         }
-                              })
+                              }                              
 
                               // clear textbox after adding task in the list
                               event.target.reset();
@@ -81,17 +95,19 @@ const Content = () => {
                               alert("Please do not add an empty task in the to-do list.");
                     }
 
-                    console.log("tasks: ", tasks);
           }
 
           const handleRemoveTask = (idTaken) => {
                     // use filter to remove the item
                     const updatedTasks = tasks.filter((task) => task.id !== idTaken);
-
-                    console.log("updatedTasks: ", updatedTasks);
                     
                     // update the tasks array
                     setTasks(updatedTasks);
+
+                    // update localStorage 
+                    if(tasks.length === 1){
+                              localStorage.setItem('tasksLocalStorage', JSON.stringify(updatedTasks));
+                    }
           }
 
           const handleUpdateTask = (event, idTaken) => {
@@ -114,7 +130,7 @@ const Content = () => {
                     <form id="todoForm" onSubmit={(event) => handleAddTask(event)} className="todo-input">
                               {/* name was used because name is considered as the key in the key-value pair of todo object array */}
                               {/* key: value = taskItem: eat */}
-                              <input type="text" placeholder="Type task to add..." name="taskItem" className="task-input"/>
+                              <input type="text" name="taskItem" placeholder="Type task to add..." className="task-input"/>
                     </form>
                     <button form="todoForm">
                               Add Task
